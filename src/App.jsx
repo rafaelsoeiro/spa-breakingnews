63e4b98994a5f0fb45b0bd33b055d89e6bb4133b
rafaelsoeiro <103294767+rafaelsoeiro@users.noreply.github.com>
2023-02-27 19:12:59 -0300

    :recycle: refactor: switched index.css file to GlobalStyle.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { news } from "./data";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
     const [count, setCount] = useState(0);
     console.log(news);
     return (
         <>
+            <GlobalStyle />
             <Home />
             <h1>App</h1>
         </>
